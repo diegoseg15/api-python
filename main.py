eb158e87a16908a39e0ca0313e1db232cb32d633
diegoseg_15 <diegofersv@gmail.com>
2024-03-07 13:50:30 -0500

    :rocket: Implement POST method

--- main.py
+++ main.py
@@ -17,5 +17,11 @@ def get_user(user_id):
         user['query']=query
     return jsonify(user), 200
 
+@app.route("/users", methods=['POST'])
+def create_user():
+    data = request.get_json()
+    data['status']='user created'
+    return jsonify(data), 201
+
 if __name__=='__main__':
     app.run(debug=True)
