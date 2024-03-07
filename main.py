f10173782031adff3ced3d053bf7dac8fc0bd86b
diegoseg_15 <diegofersv@gmail.com>
2024-03-07 13:55:53 -0500

    :wrench: Implement PUT method

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 from flask import Flask, jsonify, request
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
-
 def root():
     return "Hola mundo" 
 
 @app.route("/users/<user_id>")
-
 def get_user(user_id):
-    user={'id':user_id,'name':'test','telefono':'0987654321'}
-    #/users/2654?query=query_test
-    query= request.args.get('query')
+    user = {'id': user_id, 'name': 'test', 'telefono': '0987654321'}
+    query = request.args.get('query')
     if query:
-        user['query']=query
+        user['query'] = query
     return jsonify(user), 200
 
 @app.route("/users", methods=['POST'])
 def create_user():
     data = request.get_json()
-    data['status']='user created'
+    data['status'] = 'user created'
     return jsonify(data), 201
 
-if __name__=='__main__':
+@app.route("/users/<user_id>", methods=['PUT'])
+def update_user(user_id):
+    data = request.get_json()
+    return jsonify({'message': 'User updated successfully', 'user_id': user_id}), 200
+
+if __name__ == '__main__':
     app.run(debug=True)
